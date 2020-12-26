3b4126ccd7cfd6f5731fb0287cf71d3f9514fa19
Orhan <yilmazorhan@gmail.com>
2020-12-26 13:36:25 +0300

    rename mysql jooq and remove unused directory

---
+++ product-group-infra-data-mysql/src/main/java/tr/com/minicrm/productgroup/data/mysql/ExceptionUtils.java
@@ -0,0 +1,24 @@
+package tr.com.minicrm.productgroup.data.mysql;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+class ExceptionUtils {
+  private ExceptionUtils() {
+
+  }
+
+  public static boolean isSqlIntegrityConstraintViolationException(Throwable root) {
+    do {
+      if (isSqlIntegrityException(root)) {
+        return true;
+      }
+      root = root.getCause();
+    } while (root != null);
+    return false;
+  }
+
+  private static boolean isSqlIntegrityException(Throwable throwable) {
+    return throwable instanceof SQLIntegrityConstraintViolationException;
+  }
+
+}
